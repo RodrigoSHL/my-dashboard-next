13658def9ff104406c50055cac7eb5835cf1a26a
RodrigoSHL <shl.catalan@gmail.com>
2024-07-20 23:50:28 -0400

    FEAT: add dashboard layout
    Signed-off-by: RodrigoSHL <shl.catalan@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  return (
-    <h1>Hello world</h1>
-  );
+
+  redirect("/dashboard/counter");
 }
