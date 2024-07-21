0bb649a12a31aa87c3f018a20afd15386dd79f9e
RodrigoSHL <shl.catalan@gmail.com>
2024-07-21 01:12:56 -0400

    FEAT: separe client and server components
    Signed-off-by: RodrigoSHL <shl.catalan@gmail.com>

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,18 @@
+import { CartCounter } from "@/shopping-cart";
+
+export const metadata = {
+  title: "Shopping Cart",
+  description: "Add product in shopping cart",
+  date: "2021-07-19",
+  tags: ['react', 'state', 'local']
+}
 
 export default function CounterPage() {
   return (
-    <div>
-      <h1>Hello Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full ">
+      <span>Products in the shopping cart</span>
+      <CartCounter value={2} />
+    
     </div>
   );
 }
--- src/components/SidebarMenuItem.tsx
+++ src/components/SidebarMenuItem.tsx
@@ -1,3 +1,7 @@
+'use client';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 interface Props {
     path: string;
     icon: JSX.Element;
@@ -5,9 +9,14 @@ interface Props {
     subtitle: string;
 }
 
+
+
 export const SidebarMenuItem = ({ path, icon, title, subtitle}: Props) => {
+
+    const currentPath = usePathname();
+
     return (
-        <a href={path} className="w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3 bg-blue-800 hover:bg-white/5 transition ease-linear duration-150">
+        <Link href={path} className={`w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3 ${ (currentPath === path) && 'bg-blue-800'} hover:bg-white/5 transition ease-linear duration-150`}>
             <div>
                 {icon}
 
@@ -16,6 +25,6 @@ export const SidebarMenuItem = ({ path, icon, title, subtitle}: Props) => {
                 <span className="text-lg font-bold leading-5 text-white">{title}</span>
                 <span className="text-sm text-white/50 hidden md:block">{subtitle}</span>
             </div>
-        </a>
+        </Link>
     )
 }
