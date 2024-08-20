dd6fd47cc2fb8e04d8dbcc58b586c2170b1632f0
RodrigoSHL <shl.catalan@gmail.com>
2024-08-20 10:19:38 -0400

    FEAT: get data from store

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,29 +1,39 @@
 'use client';
 
-import{ useState } from 'react'
+import { useAppDispatch, useAppSelector } from '@/store';
+import { addOne, initCounterState, resetCount, subtractOne } from '@/store/counter/counterSlice';
+import { use, useEffect, useState } from 'react'
 
 interface Props {
     value: number
 }
 
 
-export const CartCounter = ({value = 0} : Props) => {
+export const CartCounter = ({ value = 0 }: Props) => {
 
-    const [count, setCount] = useState(value)
+    //const [count, setCount] = useState(value)
+
+    const { count } = useAppSelector(state => state.counter)
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(initCounterState(value))
+    }, [dispatch, value])
+
 
     return (
         <>
             <span className="text-9xl">{count}</span>
             <div className="flex">
-                <button 
-                    onClick={() => setCount(count + 1)} 
+                <button
+                    onClick={() => dispatch(addOne())}
                     className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
-                        +1
+                    +1
                 </button>
-                <button 
-                    onClick={() => setCount(count - 1)} 
+                <button
+                    onClick={() => dispatch(subtractOne())}
                     className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
-                        -1
+                    -1
                 </button>
             </div>
         </>
